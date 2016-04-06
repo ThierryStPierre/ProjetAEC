ea44380f4d85f528de0dbb90b683f85cab46835e
username <Jonathan.Bleau-Guerette.1@collegeahuntsic.qc.ca>
2016-04-06 15:24:45 -0400

    Debut des objet(test2)

--- app/src/main/java/com/example/thierry/projetaec/Objets/Equipe.java
+++ app/src/main/java/com/example/thierry/projetaec/Objets/Equipe.java
@@ -7,4 +7,43 @@ public class Equipe {
     private int idEquipe;
     private int idLigue;
     private String nomEquipe;
+
+    public Equipe(int idEquipe, int idLigue, String nomEquipe) {
+        this.idEquipe = idEquipe;
+        this.idLigue = idLigue;
+        this.nomEquipe = nomEquipe;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipe{" +
+                "idEquipe=" + idEquipe +
+                ", idLigue=" + idLigue +
+                ", nomEquipe='" + nomEquipe + '\'' +
+                '}';
+    }
+
+    public int getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(int idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    public int getIdLigue() {
+        return idLigue;
+    }
+
+    public void setIdLigue(int idLigue) {
+        this.idLigue = idLigue;
+    }
+
+    public String getNomEquipe() {
+        return nomEquipe;
+    }
+
+    public void setNomEquipe(String nomEquipe) {
+        this.nomEquipe = nomEquipe;
+    }
 }
