9ce0c7846cb27bbbff2a3310553a709bf264f689
Daniel Brisson <daniel.brisson.1@collegeahuntsic.qc.ca>
2016-04-08 13:27:09 -0400

    Continuer le layout du selection_joueur et des boutons

--- app/src/main/java/com/example/thierry/projetaec/MainActivity.java
+++ app/src/main/java/com/example/thierry/projetaec/MainActivity.java
@@ -8,13 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 // Main Activity  -- ceci est un commentaire pour valider l'échange GitHub 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_selection_joueurs);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
