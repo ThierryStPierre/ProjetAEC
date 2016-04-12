b2c1ab968d2dadb622124cac8aee56df88da1d52
Daniel Brisson <daniel.brisson.1@collegeahuntsic.qc.ca>
2016-04-12 12:55:34 -0400

    layout et fonction de selection de joueur termine, il reste a implémenter la base de donnes aux fonctions

--- app/src/main/java/com/example/thierry/projetaec/MainActivity.java
+++ app/src/main/java/com/example/thierry/projetaec/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thierry.projetaec;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,26 +11,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import com.example.thierry.projetaec.Objets.Selection_Joueurs;
+
 public class MainActivity extends AppCompatActivity {
 // Main Activity  -- ceci est un commentaire pour valider l'échange GitHub 
-
+private Button bouttonPartie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_selection_joueurs);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.acceuil);
+        bouttonPartie  = (Button) findViewById(R.id.btnPartie);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Raaaaeplace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });*/
+        bouttonPartie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle b = new Bundle();
+                Intent i = new Intent(MainActivity.this, Selection_Joueurs.class);
+                startActivity(i);
+            }
         });
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
